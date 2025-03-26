44e2991c2a097ccfee6cd84d35b9638f6e86334e
DvGuerraa <davimguerra@gmail.com>
2025-03-26 12:59:21 -0300

    PHP8+

--- carro.php
+++ carro.php
@@ -2,18 +2,10 @@
 class Carro{
     //atributos
 
-    public string $cor;
-    public int $ano;
-    public string $modelo;
-
     //métodos
 
-    public function __construct(string $cor, int $ano, string $modelo)
+    public function __construct(public string $cor, public int $ano, public string $modelo)
     {
-        $this->cor = $cor;
-        $this->ano = $ano;
-        $this->modelo = $modelo;
-
         echo 'Construindo carro '.$this->modelo.'... <br>';
     }
 
