30bae34e78e2c5fa04ca87ec3d93aa4743255011
DvGuerraa <davimguerra@gmail.com>
2025-03-28 12:10:47 -0300

    ChequeEspecial

--- conta.php
+++ conta.php
@@ -3,27 +3,34 @@
 class Conta{
     //Não é possível depositar mais de 100 reais
     //Não é possível sacar mais que o valor do saldo
-
+    //Cheque especial(Negativo ate 100 reais)
 
     //Atributos
     private int $saldo;
+    private bool $chequeEspecial;
 
     //Métodos
-    public function __construct(int $saldo)
+    public function __construct(int $saldo, bool $chequeEspecial = false)
     {
         $this->saldo = $saldo;
+        $this->chequeEspecial = $chequeEspecial;
     }
 
     public function sacar(int $valor)
     {
-        if($valor > $this->saldo)
+        if($valor > $this->saldo && $this->chequeEspecial == false)
         {
             echo 'Você não tem saldo suficiente <br>';
             return false;
-        }else{
+        }
+        
+        if($valor > $this->saldo + 100 && $this->chequeEspecial == true ){
+            echo 'Você não pode sacar mais que o saldo + cheque especial (100) <br>';
+            return false;
+        }
+        
         echo 'Você sacou: '.$valor.'<br>';
         $this->saldo -= $valor;
-        }
     }
 
     public function depositar(int $valor)
@@ -45,5 +52,16 @@ class Conta{
 $conta1 = new Conta(500);
 echo 'Saldo Inicial: '.$conta1->verSaldo(). '<br>';
 
-$conta1->depositar(100);
+$conta1->depositar(111);
+$conta1->sacar(600);
 echo $conta1->verSaldo(). '<br>';
+
+'<hr>';
+
+$conta2 = new Conta(500, true);
+echo 'Saldo Inicial: '.$conta2->verSaldo(). '<br>';
+
+$conta2->depositar(111);
+$conta2->sacar(600);
+echo $conta2->verSaldo(). '<br>';
+
