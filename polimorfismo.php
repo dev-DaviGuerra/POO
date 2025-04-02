97c9f980579736fc3a1b4ed00feba491e2d03477
DvGuerraa <davimguerra@gmail.com>
2025-04-02 12:13:48 -0300

    Polimorfismo

---
+++ polimorfismo.php
@@ -0,0 +1,51 @@
+<?php
+
+class Animal
+{
+
+    public function emitirSom()
+    {
+        return null;
+    }
+}
+
+class Cachorro extends Animal
+{
+    public $nome = 'Cachorro';
+    public function emitirSom()
+    {
+        return 'Au Au <br>';
+    }
+}
+
+class Gato extends Animal
+{
+    public $nome = 'Gato';
+    public function emitirSom()
+    {
+        return 'Miau <br>';
+    }
+}
+
+class Peixe extends Animal
+{
+    public $nome = 'Peixe';
+}
+
+
+function fazerOAnimalEmitirSom($animal)
+{
+    echo 'O animal: '.$animal->nome.' emite: '.$animal->emitirSom();
+}
+
+$cachorro = new Cachorro();
+$gato = new Gato();
+$peixe = new Peixe();
+
+
+$animais = array($cachorro, $gato, $peixe);
+
+foreach ($animais as $animal)
+{
+    fazerOAnimalEmitirSom($animal);
+}
