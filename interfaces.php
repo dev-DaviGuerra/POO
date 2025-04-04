b26842e1b3e6602bc4f95967bd6f86468612990c
DvGuerraa <davimguerra@gmail.com>
2025-04-04 11:14:20 -0300

    Interfaces2

--- interfaces.php
+++ interfaces.php
@@ -1,26 +1,51 @@
 <?php
 
-
-class Paypal
+interface MetodoPagamento
 {
-    public function pagar($valor)
+    public function pagar(float $valor): bool;
+
+}
+
+class Paypal implements MetodoPagamento
+{
+    public function pagar(float $valor):bool
     {
-        echo 'Pagamento via Paypal no valor de '. $valor.'<br>';
+        echo 'Pagamento via Paypal no valor de R$'. $valor.'<br>';
         return true;
     }
 }
 
-class CreditCard
+class CreditCard implements MetodoPagamento
 {
-    public function pagar($valor)
+    public function pagar(float $valor):bool
     {
-        echo 'Pagamento via cartão de crédito no valor de '. $valor. '<br>';
-        return 'sucesso';
+        echo 'Pagamento via cartão de crédito no valor de R$'. $valor. '<br>';
+        return true;
     }
 }
 
-$pagamento = new Paypal();
-$pagamento->pagar(100);
+class Pix implements MetodoPagamento
+{
+    private function gerarQrCode(float $valor): string{
+        echo 'Gerando QR Code para pagamento via Pix no valor de R$'.$valor.'<br>';
+        return 'QRCODE';
+    }
+    public function pagar(float $valor): bool
+    {
+        $qrCode = $this->gerarQrCode($valor);
+        echo 'Pagamento via Pix no valor de R$'.$valor.'<br>';
+        echo 'QR Code: '.$qrCode.'<br>';
+        return true;
+    }
+}
+
+$pagamento = new Pix();
+if($pagamento->pagar(100) === true)
+{
+    echo 'Liberando o produto... <br>';
+} else{
+    echo 'Rejeitado. <br>';
+}
 //Nosso siestema usando Interface
 
 //Eu quero...
